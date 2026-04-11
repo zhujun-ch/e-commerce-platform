c667656b747f0baa9ad206c72a7762524d83c86a
dhj594521 <vm9fe1@163.com>
2026-04-12 01:40:10 +0800

    chore: remove student2 and student2-frontend from repository

--- student2/src/main/java/org/example/student2/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package org.example.student2.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.example.student2.entity.User;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
--- student2/src/main/java/org/example/student2/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.example.student2.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import org.example.student2.entity.User;
-
-public interface UserService extends IService<User> {
-    User login(String username, String password);
-}
